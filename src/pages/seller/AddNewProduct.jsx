c282c253db94291dad6c2e466da55c4e98866347
Rishav Singh Dogra <rishav.vdoit@gmail.com>
2024-06-03 12:50:29 +0530

    Added rashan list and made changes in add product modal accordingly

--- src/pages/seller/AddNewProduct.jsx
+++ src/pages/seller/AddNewProduct.jsx
@@ -19,23 +19,47 @@ import {
   Button,
   useDisclosure,
 } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
+import { rashanList } from "@/configs/RashanList";
 
-const AddNewProduct = ({className}) => {
+function SelectProduct({ selectedProduct, onSelect }) {
+  return (
+    <Select
+      selectedKeys={selectedProduct}
+      onSelectionChange={onSelect}
+      aria-label="."
+      placeholder="select product"
+      className="w-full bg-white rounded-lg"
+      scrollShadowProps={{
+        isEnabled: false
+      }}
+    >
+      {rashanList.map((product) => (
+        <SelectItem key={product} className="bg-white  border border-slate-700 rounded-lg">
+          {product}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+}
+
+const AddNewProduct = ({ className }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const [productTitle, setProductTitle] = useState("");
+  console.log(isOpen);
   const [productImage, setProductImage] = useState("");
   const [price, setPrice] = useState("");
   const [productDescription, setProductDescription] = useState("");
+  const [selectedProduct, setSelectedProduct] = useState(new Set([]));
 
   const handleAddProduct = () => {
     // Validation and submission logic here
-    if (!productTitle || !productImage || !price || !productDescription) {
+    if (!productImage || !price || !productDescription) {
       alert("All fields are required!");
       return;
     }
     // Add product logic here
     console.log({
-      productTitle,
+      selectedProduct,
       productImage,
       price,
       productDescription,
@@ -55,7 +79,12 @@ const AddNewProduct = ({className}) => {
           </CardDescription>
         </CardHeader>
         <CardFooter>
-          <Button className ="h-11 rounded-md px-8 bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90" onPress={onOpen}>Add new product</Button>
+          <Button
+            className="h-11 rounded-md px-8 bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90"
+            onPress={onOpen}
+          >
+            Add new product
+          </Button>
         </CardFooter>
       </Card>
       <Modal
@@ -63,70 +92,75 @@ const AddNewProduct = ({className}) => {
         onOpenChange={onOpenChange}
         backdrop="blur"
         placement="top-center"
-        className="bg-slate-50"
+        className="bg-slate-300/90 backdrop-blur-sm rounded-xl"
       >
         <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1 border-b border-gray-200 pb-4">
-                <h2 className="text-2xl font-semibold text-gray-800">
-                  Add New Product
-                </h2>
-              </ModalHeader>
-              <ModalBody className="p-6 space-y-2">
-                <Label htmlFor="Product Title">Your Product Title</Label>
-                <Input
-                  autoFocus
-                  type="Product Title"
-                  placeholder="Enter product title"
-                  value={productTitle}
-                  onChange={(e) => setProductTitle(e.target.value)}
-                  className="border border-gray-300 rounded-lg"
-                />
-                <Label htmlFor="Product Image URL">Product Image URL</Label>
-                <Input
-                  type="Product Image URL"
-                  placeholder="Enter image URL"
-                  value={productImage}
-                  onChange={(e) => setProductImage(e.target.value)}
-                  className="border border-gray-300 rounded-lg"
-                />
-                <Label htmlFor="Price">Price</Label>
-                <Input
-                  label="Price"
-                  placeholder="Enter price"
-                  value={price}
-                  onChange={(e) => setPrice(e.target.value)}
-                  className="border border-gray-300 rounded-lg"
-                />
-                <Label htmlFor="Product Description">Product Description</Label>
-                <Textarea
-                  type="Product Description"
-                  placeholder="Enter product description"
-                  value={productDescription}
-                  onChange={(e) => setProductDescription(e.target.value)}
-                  className="border border-gray-300 rounded-lg"
-                />
-              </ModalBody>
-              <ModalFooter className="border-t border-gray-200 pt-4">
-                <Button
-                  color="danger"
-                  variant="flat"
-                  onPress={onClose}
-                  className="mr-3 bg-red-500 text-white hover:bg-red-600"
-                >
-                  Close
-                </Button>
-                <Button
-                  color="primary"
-                  onPress={handleAddProduct}
-                  className="bg-blue-500 text-white hover:bg-blue-600"
-                >
-                  Add Product
-                </Button>
-              </ModalFooter>
-            </>
-          )}
+          {(onClose) => {
+            // console.log(onclose);
+            return (
+              <>
+                <ModalHeader className="flex flex-col gap-1 border-b border-gray-200 pb-4">
+                  <h2 className="text-2xl font-semibold text-gray-800">
+                    Add New Product
+                  </h2>
+                </ModalHeader>
+                <ModalBody className="p-6 space-y-2">
+                  <Label htmlFor="Product Title">Select product to add</Label>
+                  <SelectProduct
+                    type="Product Title"
+                    selectedProduct={selectedProduct}
+                    onSelect={setSelectedProduct}
+                  />
+                  <Label htmlFor="Product Image URL">Product Image URL</Label>
+                  <Input
+                    type="Product Image URL"
+                    placeholder="Enter image URL"
+                    value={productImage}
+                    onChange={(e) => setProductImage(e.target.value)}
+                    className="border border-gray-300 rounded-lg"
+                  />
+                  <Label htmlFor="Price">Price</Label>
+                  <Input
+                    label="Price"
+                    placeholder="Enter price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    className="border border-gray-300 rounded-lg"
+                  />
+                  <Label htmlFor="Product Description">
+                    Product Description
+                  </Label>
+                  <Textarea
+                    type="Product Description"
+                    placeholder="Enter product description"
+                    value={productDescription}
+                    onChange={(e) => setProductDescription(e.target.value)}
+                    className="border border-gray-300 rounded-lg"
+                  />
+                </ModalBody>
+                <ModalFooter className="border-t border-gray-200 pt-4">
+                  <Button
+                    color="danger"
+                    variant="flat"
+                    onPress={() => {
+                      console.log("Close button pressed");
+                      onClose();
+                    }}
+                    className="mr-3 bg-red-500 rounded-lg text-white hover:bg-red-600"
+                  >
+                    Close
+                  </Button>
+                  <Button
+                    color="primary"
+                    onPress={handleAddProduct}
+                    className="bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                  >
+                    Add Product
+                  </Button>
+                </ModalFooter>
+              </>
+            );
+          }}
         </ModalContent>
       </Modal>
     </>
--- src/pages/seller/Dashboard.jsx
+++ src/pages/seller/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function SellerDashboard() {
   return (
     <div className="bg-gradient-to-r from-[#7882f3] to-[#5555ee] min-h-screen">
       <Sidebar userType="seller" />
-      <AddNewProduct className={"max-w-[50%] relative left-20 top-6"} />
+      <AddNewProduct className={"max-w-[50%] relative left-20 top-6 rounded-lg"} />
     </div>
   );
 }
